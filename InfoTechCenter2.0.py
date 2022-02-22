06b5e3fc9a9732d2bdf8eab356f142099f006eaf
Christopher <02christopher.villar@gulllakecs.org>
2022-02-22 13:27:25 -0500

    Imported time library and used the sleep function.

---
+++ InfoTechCenter2.0.py
@@ -0,0 +1,6 @@
+# Code Name - Hornet
+
+from time import sleep #Print to one line with time delay between prints
+
+print("Welcome to Hornets InfoTechCenter\n")  ; sleep(1)
+print("Hornet's Operating System Booting Up")
